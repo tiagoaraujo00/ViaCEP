18608ba9a9fadb2586a0fcc3b00f1c04152c0708
tiagoaraujo00 <tiago81451@gmail.com>
2024-06-13 23:34:20 -0300

    feat: add SearchBarUI component
    Added a new SearchBarUI component that includes a search bar with a leading search icon, a trailing close icon, and customizable query and placeholder text. This component utilizes Material 3 API and supports query state management through a callback.

---
+++ app/src/main/java/com/tiagoaraujo00/viacep/components/SearchBarUI.kt
@@ -0,0 +1,95 @@
+package com.tiagoaraujo00.viacep.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBarUI(
+    modifier: Modifier = Modifier,
+    query: String = "",
+    placeholder: String = "",
+    onQueryChange: (String) -> Unit
+) {
+    SearchBar(
+        modifier = modifier.fillMaxWidth(),
+        query = query,
+        onQueryChange = {
+            onQueryChange(it)
+        },
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        placeholder = {
+            Text(
+                text = placeholder,
+                color = Color.Gray
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = Color.Gray
+            )
+        },
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = null,
+                modifier = Modifier.clickable {
+                    onQueryChange("")
+                },
+                tint = Color.Gray
+            )
+        },
+        shape = RoundedCornerShape(0.dp),
+        colors = SearchBarDefaults.colors(
+            containerColor = Color.LightGray,
+        ),
+        content = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchBarUIPreview() {
+    var query by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        SearchBarUI(
+            query = query,
+            onQueryChange = {
+                query = it
+            },
+            placeholder = "Ex: Av. Paulista",
+        )
+    }
+}
